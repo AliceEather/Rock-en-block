11d2f7457a1b67cc5f6cf54308da62bc56661842
AliceEather <alice.lehmann@hotmail.fr>
2017-10-18 17:59:49 +0200

    Jour n°3

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+  $(".button").click(function(){
+        $("header").toggle(1000);
+
+});
+
+});
+$.getJSON("https://api.airtable.com/v0/apploRqA5suojS81Q/musics?api_key=keyrIlRdQy0VIxqqR",
+					function(data) {
+              console.log(data);
+              $.each(data.records, function(key,val) {
+                  console.log(val.fields.title)
+                  $("<iframe src='https://open.spotify.com/embed?uri="+val.fields['spotify-link']+"&view=coverart frameborder='0' allowtransparency='true'></iframe>").appendTo('#musiques');
+
+              });
+
+					}
+);
