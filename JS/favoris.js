18acc97ec7b5a0171f8c49668994a20160e7227a
M-pidu5-9-1-00 <32840120+M-pidu5-9-1-00@users.noreply.github.com>
2017-10-20 11:39:41 +0200

    Jour 5 n°2

---
+++ JS/favoris.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+
+  //appel la Fonction filterMusic déclaré dans script.js
+filterMusic('Coup de Coeur', '#musiques');
+
+});
--- JS/script.js
+++ JS/script.js
@@ -14,32 +14,6 @@ function filterMusic(genre, div) {
 
 $(document).ready(function(){
 
-  //$(".recherche").click(function(){
-        //$("recherche").toggle(1000);
-
-  //});
-
-// On la met de côté pour ne pas la perdre
-
-// $.getJSON("https://api.airtable.com/v0/apploRqA5suojS81Q/musics?api_key=keyrIlRdQy0VIxqqR",
-// 					function(data) {
-//               console.log(data);
-//               $.each(data.records, function(key,val) {
-//                   console.log(val.fields.title)
-//                   $("<iframe src='https://open.spotify.com/embed?uri="+val.fields['spotify-link']+"&view=coverart' frameborder='0' allowtransparency='true'></iframe>").appendTo('#musiques');
-//
-// });
-
-// $.getJSON("https://api.airtable.com/v0/apploRqA5suojS81Q/musics?api_key=keyrIlRdQy0VIxqqR",
-// 					function(data) {
-//               $.each(data.records,function(key,val) {
-//               $(val.fields.title).appendTo('#hardrock');
-//               $("<iframe src='https://open.spotify.com/embed?uri="+val.fields['spotify-link']+"&view=coverart' frameborder='0' allowtransparency='true'></iframe>").appendTo('#hardrock');
-//             });
-// });
-
-
-
 
 $('#search').on("click",function() {
       $('#musiques').html(''); //Va chercher l'article #musiques
